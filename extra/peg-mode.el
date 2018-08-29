728077be727cee52e84bdb59d0f90fcb25da1afc
Lincoln de Sousa <lincoln@comum.org>
2018-08-29 11:26:19 -0400

    chore(editor): Add license to Emacs syntax highlight for PEGs

--- extra/peg-mode.el
+++ extra/peg-mode.el
@@ -6,6 +6,19 @@
 ;; Version: 0.0.1
 ;; Homepage: https://github.com/clarete/emacs.d
 ;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;
 ;;; Commentary:
 ;;
 ;; Didn't really find any mode for PEGs that I liked.  Here's what I

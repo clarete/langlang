2b59ffeef8304e30c4705bea8b32633ca013817b
Lincoln de Sousa <lincoln@clarete.li>
2021-11-14 15:56:02 -0500

    chore: move code from main to separate binaries ll{cc,sh}

---
+++ src/bin/llcc.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use langlang::{parser, vm};
+
+fn run_grammar_on_input_from_cmd() -> Result<(), std::io::Error> {
+    let grammar_file = std::env::args().nth(1).expect("no grammar given");
+    let input_file = std::env::args().nth(2).expect("no input given");
+    let grammar_data = fs::read_to_string(grammar_file)?;
+    let mut c = parser::Compiler::new();
+    if let Err(e) = c.compile_str(grammar_data.as_str()) {
+        return Err(std::io::Error::new(std::io::ErrorKind::NotFound, e.to_string()));
+    }
+
+    let p = c.program();
+    println!("Compiled:\n{}", p.to_string());
+
+    let input_data = fs::read_to_string(input_file)?;
+    let mut m = vm::VM::new(p);
+    match m.run(&input_data) {
+        Ok(Some(v)) => println!("{:#?}", v),
+        Ok(None) => println!("not much"),
+        Err(e) => println!("{:?}", e),
+    }
+    Ok(())
+}
+
+fn main() {
+    env_logger::init();
+
+    if let Err(e) = run_grammar_on_input_from_cmd() {
+        println!("{}", e.to_string());
+    }
+}
---
+++ src/bin/llsh.rs
@@ -0,0 +1,95 @@
+use std::fs;
+use std::io::{self, Write};
+
+use langlang::{parser, vm};
+
+#[derive(Debug)]
+pub enum ShellError {
+    ParsingError(parser::Error),
+    RuntimeError(vm::Error),
+    IOError(io::Error),
+}
+
+impl std::fmt::Display for ShellError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            ShellError::ParsingError(e) => write!(f, "Parsing Error: {:#?}", e),
+            ShellError::RuntimeError(e) => write!(f, "Runtime Error: {:#?}", e),
+            ShellError::IOError(e) => write!(f, "Input/Output Error: {:#?}", e),
+        }
+    }
+}
+
+impl std::error::Error for ShellError {}
+
+impl From<io::Error> for ShellError {
+    fn from(e: io::Error) -> Self {
+        ShellError::IOError(e)
+    }
+}
+
+impl From<parser::Error> for ShellError {
+    fn from(e: parser::Error) -> Self {
+        ShellError::ParsingError(e)
+    }
+}
+
+impl From<vm::Error> for ShellError {
+    fn from(e: vm::Error) -> Self {
+        ShellError::RuntimeError(e)
+    }
+}
+
+fn shell() -> Result<(), ShellError> {
+    let file_name = std::env::args().nth(1).expect("no grammar given");
+    let data = fs::read_to_string(&file_name)?;
+
+    println!("welcome to langlang. use Ctrl-D to get outta here.");
+    println!("loaded: {}", file_name);
+
+    let mut c = parser::Compiler::new();
+    c.compile_str(data.as_str())?;
+    let p = c.program();
+
+    loop {
+        // display prompt
+        print!("langlang% ");
+        io::stdout().flush().expect("can't flush stdout");
+
+        // read the next line typed in
+        let mut line = String::new();
+        io::stdin().read_line(&mut line)?;
+
+        // handle Ctrl-D
+        if line.as_str() == "" {
+            println!();
+            break;
+        }
+
+        // skip empty lines
+        if line.as_str() == "\n" {
+            continue;
+        }
+
+        // removed the unwanted last \n
+        line.pop();
+
+        // run the line
+        let mut m = vm::VM::new(p.clone());
+        match m.run(&line) {
+            Ok(Some(v)) => println!("{:#?}", v),
+            Ok(None) => println!("not much"),
+            Err(e) => return Err(ShellError::RuntimeError(e)),
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    env_logger::init();
+
+    if let Err(e) = shell() {
+        println!("{}", e.to_string());
+    }
+}

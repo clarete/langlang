00d317e66757e574f7f52cadd97000fe232e405f
Lincoln de Sousa <lincoln@clarete.li>
2022-08-31 19:17:06 -0400

    parser: factor out ast from the parser module

---
+++ src/ast.rs
@@ -0,0 +1,33 @@
+// TODO: attach Location to AST nodes
+//
+// #[derive(Debug)]
+// pub struct Location {
+//     // how many characters have been seen since the begining of
+//     // parsing
+//     cursor: usize,
+//     // how many end-of-line sequences seen since the begining of
+//     // parsing
+//     line: usize,
+//     // how many characters seen since the begining of the line
+//     column: usize,
+// }
+
+#[derive(Clone, Debug, Hash, PartialEq)]
+pub enum AST {
+    Grammar(Vec<AST>),
+    Definition(String, Box<AST>),
+    LabelDefinition(String, String),
+    Sequence(Vec<AST>),
+    Choice(Vec<AST>),
+    Not(Box<AST>),
+    Optional(Box<AST>),
+    ZeroOrMore(Box<AST>),
+    OneOrMore(Box<AST>),
+    Identifier(String),
+    Str(String),
+    Range(char, char),
+    Char(char),
+    Label(String, Box<AST>),
+    Any,
+    Empty,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod ast;
 pub mod parser;
 pub mod vm;
 pub mod format;

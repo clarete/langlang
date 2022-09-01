bd32ad9a485abb540faf2c866066640e81d24e63
Lincoln de Sousa <lincoln@clarete.li>
2022-08-31 20:18:21 -0400

    llcc: rust-fmt

--- src/bin/llcc.rs
+++ src/bin/llcc.rs
@@ -20,25 +20,34 @@ fn formatter(name: &str) -> FormattingFunc {
 fn run_grammar_on_input_from_cmd() -> Result<(), std::io::Error> {
     let grammar_file = std::env::args().nth(1).expect("no grammar given");
     let input_file = std::env::args().nth(2).expect("no input given");
-    let fmt = formatter(std::env::args().nth(3).unwrap_or("fmt0".to_string()).as_str());
+    let fmt = formatter(
+        std::env::args()
+            .nth(3)
+            .unwrap_or("fmt0".to_string())
+            .as_str(),
+    );
     let grammar_data = fs::read_to_string(grammar_file)?;
 
     let mut p = parser::Parser::new(grammar_data.as_str());
     let ast = match p.parse_grammar() {
         Ok(a) => a,
-        Err(e) => return Err(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            e.to_string(),
-        )),
+        Err(e) => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                e.to_string(),
+            ))
+        }
     };
 
     let mut c = compiler::Compiler::default();
     let program = match c.compile(ast) {
         Ok(p) => p,
-        Err(e) => return Err(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            e.to_string(),
-        )),
+        Err(e) => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                e.to_string(),
+            ))
+        }
     };
 
     println!("Compiled:\n{}", program);
@@ -46,7 +55,7 @@ fn run_grammar_on_input_from_cmd() -> Result<(), std::io::Error> {
     let input_data = fs::read_to_string(input_file)?;
     let mut m = vm::VM::new(program);
     match m.run(&input_data) {
-        Ok(Some(v)) => println!("{}", fmt(v)),
+        Ok(Some(v)) => println!("{}", fmt(&v)),
         Ok(None) => println!("not much"),
         Err(e) => println!("{:?}", e),
     }

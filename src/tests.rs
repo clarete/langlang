682d155b93fc9e3858dc3b0d8817c15233c54113
Lincoln de Sousa <lincoln@clarete.li>
2022-11-12 18:32:35 -0500

    tests: add helper to run VM tests for lists

--- src/tests.rs
+++ src/tests.rs
@@ -3,31 +3,6 @@ mod tests {
     use crate::{compiler, format, parser, vm};
     use log::debug;
 
-    fn compile(cc: &compiler::Config, grammar: &str) -> vm::Program {
-        let mut p = parser::Parser::new(grammar);
-        let ast = p.parse().unwrap();
-        let mut c = compiler::Compiler::new(cc.clone());
-        let program = c.compile(ast).unwrap();
-        debug!("p: {}", program);
-        program
-    }
-
-    fn run(program: &vm::Program, input: &str) -> Option<vm::Value> {
-        let mut machine = vm::VM::new(program);
-        machine.run_str(input).expect("Unexpected")
-    }
-
-    fn cc_run(cc: &compiler::Config, grammar: &str, input: &str) -> Option<vm::Value> {
-        let prog = compile(cc, grammar);
-        let mut machine = vm::VM::new(&prog);
-        machine.run_str(input).expect("Unexpected")
-    }
-
-    fn assert_success(expected: &str, value: Option<vm::Value>) {
-        assert!(value.is_some());
-        assert_eq!(expected.to_string(), format::value_fmt1(&value.unwrap()));
-    }
-
     #[test]
     fn test_char() {
         let cc = compiler::Config::default();
@@ -37,9 +12,8 @@ mod tests {
     #[test]
     fn test_str() {
         let cc = compiler::Config::default();
-        let value = vm::VM::new(&compile(&cc, "A <- 'abada'"))
-            .run(vec![vm::Value::Str("abada".to_string())])
-            .unwrap();
+        let p = compile(&cc, "A <- 'abada'");
+        let value = run(&p, vec![vm::Value::Str("abada".to_string())]).unwrap();
         assert_success("A[abada]", value);
     }
 
@@ -98,27 +72,27 @@ mod tests {
     fn test_var_ending_with_zero_or_more() {
         let cc = compiler::Config::default();
         let program = compile(&cc, "A <- '1'*");
-        assert_success("A[111]", run(&program, "111"));
-        assert_success("A[11]", run(&program, "11"));
-        assert_success("A[1]", run(&program, "1"));
-        assert!(run(&program, "").is_none())
+        assert_success("A[111]", run_str(&program, "111"));
+        assert_success("A[11]", run_str(&program, "11"));
+        assert_success("A[1]", run_str(&program, "1"));
+        assert!(run_str(&program, "").is_none())
     }
 
     #[test]
     fn test_var_ending_with_one_or_more() {
         let cc = compiler::Config::default();
         let program = compile(&cc, "A <- '1'+");
-        assert_success("A[111]", run(&program, "111"));
-        assert_success("A[11]", run(&program, "11"));
-        assert_success("A[1]", run(&program, "1"));
+        assert_success("A[111]", run_str(&program, "111"));
+        assert_success("A[11]", run_str(&program, "11"));
+        assert_success("A[1]", run_str(&program, "1"));
     }
 
     #[test]
     fn test_var_ending_with_option() {
         let cc = compiler::Config::default();
         let program = compile(&cc, "A <- '1' '1'?");
-        assert_success("A[11]", run(&program, "11"));
-        assert_success("A[1]", run(&program, "1"));
+        assert_success("A[11]", run_str(&program, "11"));
+        assert_success("A[1]", run_str(&program, "1"));
     }
 
     // -- Unicode --------------------------------------------------------------
@@ -136,19 +110,19 @@ mod tests {
     fn test_lr0() {
         let cc = compiler::Config::default();
         let program = compile(&cc, "E <- E '+n' / 'n'");
-        assert_success("E[n]", run(&program, "n"));
-        assert_success("E[E[n]+n]", run(&program, "n+n"));
-        assert_success("E[E[E[n]+n]+n]", run(&program, "n+n+n"));
+        assert_success("E[n]", run_str(&program, "n"));
+        assert_success("E[E[n]+n]", run_str(&program, "n+n"));
+        assert_success("E[E[E[n]+n]+n]", run_str(&program, "n+n+n"));
     }
 
     #[test]
     fn test_lr1() {
         let cc = compiler::Config::default();
         let program = compile(&cc, "E <- E '+' E / 'n'+");
-        assert_success("E[n]", run(&program, "n"));
-        assert_success("E[E[n]+E[n]]", run(&program, "n+n"));
-        assert_success("E[E[n]+E[E[n]+E[n]]]", run(&program, "n+n+n"));
-        assert_success("E[E[n]+E[E[n]+E[E[n]+E[n]]]]", run(&program, "n+n+n+n"));
+        assert_success("E[n]", run_str(&program, "n"));
+        assert_success("E[E[n]+E[n]]", run_str(&program, "n+n"));
+        assert_success("E[E[n]+E[E[n]+E[n]]]", run_str(&program, "n+n+n"));
+        assert_success("E[E[n]+E[E[n]+E[E[n]+E[n]]]]", run_str(&program, "n+n+n+n"));
     }
 
     #[test]
@@ -161,10 +135,10 @@ mod tests {
              M <- M '-n' / 'n'
             ",
         );
-        assert_success("E[M[n]]", run(&program, "n"));
-        assert_success("E[M[M[n]-n]]", run(&program, "n-n"));
-        assert_success("E[M[M[M[n]-n]-n]]", run(&program, "n-n-n"));
-        assert_success("E[M[n]+E[M[n]+E[M[n]]]]", run(&program, "n+n+n"));
+        assert_success("E[M[n]]", run_str(&program, "n"));
+        assert_success("E[M[M[n]-n]]", run_str(&program, "n-n"));
+        assert_success("E[M[M[M[n]-n]-n]]", run_str(&program, "n-n-n"));
+        assert_success("E[M[n]+E[M[n]+E[M[n]]]]", run_str(&program, "n+n+n"));
     }
 
     #[test]
@@ -182,20 +156,20 @@ mod tests {
         );
         // Right associative, as E is both left and right recursive,
         // without precedence
-        assert_success("E[n]", run(&program, "n"));
-        assert_success("E[E[n]+E[n]]", run(&program, "n+n"));
-        assert_success("E[E[n]+E[E[n]+E[n]]]", run(&program, "n+n+n"));
-        assert_success("E[E[n]-E[n]]", run(&program, "n-n"));
-        assert_success("E[E[n]-E[E[n]-E[n]]]", run(&program, "n-n-n"));
-        assert_success("E[E[n]*E[n]]", run(&program, "n*n"));
-        assert_success("E[E[n]*E[E[n]*E[n]]]", run(&program, "n*n*n"));
-        assert_success("E[E[n]/E[n]]", run(&program, "n/n"));
-        assert_success("E[E[n]/E[E[n]/E[n]]]", run(&program, "n/n/n"));
-        assert_success("E[E[n]-E[E[n]+E[n]]]", run(&program, "n-n+n"));
-        assert_success("E[E[n]+E[E[n]-E[n]]]", run(&program, "n+n-n"));
-        assert_success("E[E[n]+E[E[n]*E[n]]]", run(&program, "n+n*n"));
-        assert_success("E[E[n]*E[E[n]+E[n]]]", run(&program, "n*n+n"));
-        assert_success("E[E[n]/E[E[n]+E[n]]]", run(&program, "n/n+n"));
+        assert_success("E[n]", run_str(&program, "n"));
+        assert_success("E[E[n]+E[n]]", run_str(&program, "n+n"));
+        assert_success("E[E[n]+E[E[n]+E[n]]]", run_str(&program, "n+n+n"));
+        assert_success("E[E[n]-E[n]]", run_str(&program, "n-n"));
+        assert_success("E[E[n]-E[E[n]-E[n]]]", run_str(&program, "n-n-n"));
+        assert_success("E[E[n]*E[n]]", run_str(&program, "n*n"));
+        assert_success("E[E[n]*E[E[n]*E[n]]]", run_str(&program, "n*n*n"));
+        assert_success("E[E[n]/E[n]]", run_str(&program, "n/n"));
+        assert_success("E[E[n]/E[E[n]/E[n]]]", run_str(&program, "n/n/n"));
+        assert_success("E[E[n]-E[E[n]+E[n]]]", run_str(&program, "n-n+n"));
+        assert_success("E[E[n]+E[E[n]-E[n]]]", run_str(&program, "n+n-n"));
+        assert_success("E[E[n]+E[E[n]*E[n]]]", run_str(&program, "n+n*n"));
+        assert_success("E[E[n]*E[E[n]+E[n]]]", run_str(&program, "n*n+n"));
+        assert_success("E[E[n]/E[E[n]+E[n]]]", run_str(&program, "n/n+n"));
     }
 
     #[test]
@@ -215,20 +189,20 @@ mod tests {
         );
 
         // left associative with different precedences
-        assert_success("E[21]", run(&program, "21"));
-        assert_success("E[E[3]+E[5]]", run(&program, "3+5"));
-        assert_success("E[E[3]-E[5]]", run(&program, "3-5"));
+        assert_success("E[21]", run_str(&program, "21"));
+        assert_success("E[E[3]+E[5]]", run_str(&program, "3+5"));
+        assert_success("E[E[3]-E[5]]", run_str(&program, "3-5"));
         // same precedence between addition (+) and subtraction (-)
-        assert_success("E[E[E[3]-E[5]]+E[2]]", run(&program, "3-5+2"));
-        assert_success("E[E[E[3]+E[5]]-E[2]]", run(&program, "3+5-2"));
+        assert_success("E[E[E[3]-E[5]]+E[2]]", run_str(&program, "3-5+2"));
+        assert_success("E[E[E[3]+E[5]]-E[2]]", run_str(&program, "3+5-2"));
         // higher precedence for multiplication (*) over addition (+) and subtraction (-)
-        assert_success("E[E[3]+E[E[5]*E[2]]]", run(&program, "3+5*2"));
-        assert_success("E[E[E[5]*E[2]]-E[3]]", run(&program, "5*2-3"));
-        assert_success("E[E[E[E[1]*E[5]]*E[2]]+E[3]]", run(&program, "1*5*2+3"));
+        assert_success("E[E[3]+E[E[5]*E[2]]]", run_str(&program, "3+5*2"));
+        assert_success("E[E[E[5]*E[2]]-E[3]]", run_str(&program, "5*2-3"));
+        assert_success("E[E[E[E[1]*E[5]]*E[2]]+E[3]]", run_str(&program, "1*5*2+3"));
         // unary operator
-        assert_success("E[-E[1]]", run(&program, "-1"));
+        assert_success("E[-E[1]]", run_str(&program, "-1"));
         // highest precedence parenthesis
-        assert_success("E[E[(E[E[3]+E[5]])]*E[2]]", run(&program, "(3+5)*2"));
+        assert_success("E[E[(E[E[3]+E[5]])]*E[2]]", run_str(&program, "(3+5)*2"));
     }
 
     #[test]
@@ -253,11 +227,14 @@ mod tests {
     fn test_list_with_no_list() {
         let cc = compiler::Config::default();
         let program = compile(&cc, "A <- { 'aba' }");
-        let result = vm::VM::new(&program).run(vec![
-            vm::Value::Chr('a'),
-            vm::Value::Chr('b'),
-            vm::Value::Chr('a'),
-        ]);
+        let result = run(
+            &program,
+            vec![
+                vm::Value::Chr('a'),
+                vm::Value::Chr('b'),
+                vm::Value::Chr('a'),
+            ],
+        );
         assert!(result.is_err());
         assert_eq!(
             result.unwrap_err(),
@@ -275,10 +252,10 @@ mod tests {
             vm::Value::Chr('b'),
             vm::Value::Chr('a'),
         ])];
-        assert_success("A[[aba]]", vm::VM::new(&p).run(input_with_chr).unwrap());
+        assert_success("A[[aba]]", run(&p, input_with_chr).unwrap());
 
         let input_with_str = vec![vm::Value::List(vec![vm::Value::Str("aba".to_string())])];
-        assert_success("A[[aba]]", vm::VM::new(&p).run(input_with_str).unwrap())
+        assert_success("A[[aba]]", run(&p, input_with_str).unwrap())
     }
 
     #[test]
@@ -297,19 +274,13 @@ mod tests {
             vm::Value::Chr('t'),
             vm::Value::Chr('e'),
         ])];
-        assert_success(
-            "A[[[aba]cate]]",
-            vm::VM::new(&p).run(input_with_chr).unwrap(),
-        );
+        assert_success("A[[[aba]cate]]", run(&p, input_with_chr).unwrap());
 
         let input_with_str = vec![vm::Value::List(vec![
             vm::Value::List(vec![vm::Value::Str("aba".to_string())]),
             vm::Value::Str("cate".to_string()),
         ])];
-        assert_success(
-            "A[[[aba]cate]]",
-            vm::VM::new(&p).run(input_with_str).unwrap(),
-        );
+        assert_success("A[[[aba]cate]]", run(&p, input_with_str).unwrap());
     }
 
     // -- Expand Grammar -------------------------------------------------------
@@ -321,7 +292,7 @@ mod tests {
         // Program that parses the initial input
         let input_grammar = "A <- 'F'";
         let program = compile(&cc, input_grammar);
-        let output = run(&program, "F");
+        let output = run_str(&program, "F");
 
         // Program that parses the output obtained upon successful
         // parsing with the initial program
@@ -329,10 +300,40 @@ mod tests {
         let rewrite = parser::expand(original_ast)?;
         let mut c = compiler::Compiler::new(cc);
         let list_program = c.compile(rewrite).unwrap();
-        let value = vm::VM::new(&list_program)
-            .run(vec![output.unwrap()])
-            .unwrap();
+        let value = run(&list_program, vec![output.unwrap()]).unwrap();
         assert_success("A[A[F]]", value);
         Ok(())
+    }
+
+    // -- Test Helpers ---------------------------------------------------------
+
+    fn compile(cc: &compiler::Config, grammar: &str) -> vm::Program {
+        let mut p = parser::Parser::new(grammar);
+        let ast = p.parse().unwrap();
+        let mut c = compiler::Compiler::new(cc.clone());
+        let program = c.compile(ast).unwrap();
+        debug!("p: {}", program);
+        program
+    }
+
+    fn run_str(program: &vm::Program, input: &str) -> Option<vm::Value> {
+        let mut machine = vm::VM::new(program);
+        machine.run_str(input).expect("Unexpected")
+    }
+
+    fn run(program: &vm::Program, input: Vec<vm::Value>) -> Result<Option<vm::Value>, vm::Error> {
+        let mut machine = vm::VM::new(program);
+        machine.run(input)
+    }
+
+    fn cc_run(cc: &compiler::Config, grammar: &str, input: &str) -> Option<vm::Value> {
+        let prog = compile(cc, grammar);
+        let mut machine = vm::VM::new(&prog);
+        machine.run_str(input).expect("Unexpected")
+    }
+
+    fn assert_success(expected: &str, value: Option<vm::Value>) {
+        assert!(value.is_some());
+        assert_eq!(expected.to_string(), format::value_fmt1(&value.unwrap()));
     }
 }

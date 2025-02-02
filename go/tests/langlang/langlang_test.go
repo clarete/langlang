7bfc074114a659dde979d20bdb105e8e32bb3b73
Lincoln Clarete <lincoln@clarete.li>
2025-02-02 16:43:18 -0500

    go: New benchmark with langlang grammar
    There's some performance work that needs to be done, and before
    starting that work, we needed better tests.  Since the most
    sophisticated grammar so far is langlang's, we're measuring how that
    grammar is performing by running it against a few other grammars, then
    against itself.
    
    The existing benchmarks of previous versions were moved into a new
    suite directory, and we now have the "import" and "langlang" suites.

---
+++ go/tests/langlang/langlang_test.go
@@ -0,0 +1,38 @@
+package parser
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+//go:generate go run ../../cmd -language go -grammar ../../../grammars/langlang.peg -output ./langlang.go
+
+func BenchmarkParser(b *testing.B) {
+
+	grammarNames := []string{"csv", "json", "peg", "langlang"}
+
+	grammars := make(map[string]string, len(grammarNames))
+
+	read := func(n string) string {
+		text, err := os.ReadFile(fmt.Sprintf("../../../grammars/%s.peg", n))
+		require.NoError(b, err)
+		return string(text)
+	}
+
+	for _, name := range grammarNames {
+		grammars[name] = read(name)
+	}
+
+	for _, name := range grammarNames {
+		b.Run(fmt.Sprintf("Grammar %s", name), func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				p := NewParser()
+				p.SetInput(grammars[name])
+				p.ParseGrammar()
+			}
+		})
+	}
+}

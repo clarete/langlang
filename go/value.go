1a2117b8a7fb872264f83b938e578fa6a36780a5
Lincoln Clarete <lincoln@clarete.li>
2025-11-20 19:09:17 -0500

    go[value]: Add Range.Contains utility method

--- go/value.go
+++ go/value.go
@@ -28,6 +28,10 @@ const eof = -1
 // represent a position within the input.
 type Range struct{ Start, End int }
 
+func NewRange(start, end int) Range {
+	return Range{Start: start, End: end}
+}
+
 func (r Range) String() string {
 	if r.Start == r.End {
 		return fmt.Sprintf("%d", r.Start)
@@ -35,12 +39,12 @@ func (r Range) String() string {
 	return fmt.Sprintf("%d..%d", r.Start, r.End)
 }
 
-func NewRange(start, end int) Range {
-	return Range{Start: start, End: end}
-}
-
 func (r Range) Str(v []byte) string {
 	return string(v[r.Start:r.End])
+}
+
+func (r Range) Contains(other Range) bool {
+	return other.Start >= r.Start && other.End <= r.End
 }
 
 type Value interface {

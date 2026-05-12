10ef9f44d8d14c74d03f264af0fc635a58640347
Lincoln Clarete <lincoln@clarete.li>
2026-05-12 00:06:36 -0400

    docs: rebuild website

---
+++ docs/assets/examples/presets.ts
@@ -0,0 +1,6 @@
+import jsonStrippedGrammar from "./json/json.stripped.peg?raw";
+import jsonSmallInput from "./json/small.json?raw";
+import csvGrammar from "./csv/csv.peg?raw";
+import csvMissionsInput from "./csv/missions.csv?raw";
+
+export { jsonStrippedGrammar, jsonSmallInput, csvGrammar, csvMissionsInput };

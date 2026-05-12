8f74d41ee9254ed7aac3101e8f50c11f63a02b32
Lincoln Clarete <lincoln@clarete.li>
2026-05-12 00:47:33 -0400

    js: add presets missed in previous commits

---
+++ js/web/src/examples/presets.ts
@@ -0,0 +1,5 @@
+import jsonStrippedGrammar from "./json/json.stripped.peg?raw";
+import jsonSmallInput from "./json/small.json?raw";
+import csvGrammar from "./csv/csv.peg?raw";
+
+export { jsonStrippedGrammar, jsonSmallInput, csvGrammar };
